d304914a50ad0367b517d4d317646fabacbaf927
Spyingwind <spyingwind@gmail.com>
2020-12-16 16:59:16 -0600

    Adds extra check for update Adds status code for systemd

--- update-crewlink.sh
+++ update-crewlink.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 base=/opt/crewlink-server
+status=0
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root. sudo ./setup.sh"
   exit 1
 fi
 pushd $base
-if [ "$(git rev-list HEAD...origin/master --count)" -gt "0" ]; then
-    service crewlink-* stop
-    su -c "git fetch" crewlink
-    chown -R crewlink:crewlink $base
-    service crewlink-* start
+journalctl -u crewlink-9736 --since=-1h | grep -Fxq "\"GET \/\d+.\d+.\d+.yml HTTP\/1.1\" 404" | tail -n 1
+if ["$(journalctl -u crewlink-9736 --since=-1h | awk '{print ($0 ~ /\"GET \/[0-9]+.[0-9]+.[0-9]+.yml HTTP\/1.1\" 404/)?1:0}' | tail -n 1)" -eq 1] || [ "$(git rev-list HEAD...origin/master --count)" -gt "0" ]; then
+  echo "Updating CrewLink-Server"
+  service crewlink-* stop
+  su -c "git fetch --all;git reset --hard origin/master" crewlink
+  chown -R crewlink:crewlink $base
+  service crewlink-* start
+fi
+
+if [[ "$(journalctl -u crewlink-9736 --since=-1h | tail -n 4)" =~ .*"CrewLink Server started".* ]]; then
+  echo "CrewLink Server started."
+  status=0
+else
+  echo "CrewLink failed to start."
+  status=1
 fi
 popd
+exit $status
