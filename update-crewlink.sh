4d5265f8308ae9e00cbc0c4e91895b082f99378c
Spyingwind <spyingwind@gmail.com>
2020-12-18 18:09:54 -0600

    Gets logs correctly

--- update-crewlink.sh
+++ update-crewlink.sh
@@ -8,14 +8,13 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 pushd $base
-journalctl -u crewlink-9736 --since=-1h | grep -Fxq "\"GET \/\d+.\d+.\d+.yml HTTP\/1.1\" 404" | tail -n 1
-if [ "$(journalctl -u crewlink-9736 --since=-1h | awk '{print ($0 ~ /\"GET \/[0-9]+.[0-9]+.[0-9]+.yml HTTP\/1.1\" 404/)?1:0}' | tail -n 1)" -eq 1 ] || [ "$(git rev-list HEAD...origin/master --count)" -gt "0" ]; then
+if [ "$(journalctl -u crewlink-\*.service --since=-1h | awk '{print ($0 ~ /\"GET \/[0-9]+.[0-9]+.[0-9]+.yml HTTP\/1.1\" 404/)?1:0}' | tail -n 1)" -eq 1 ] || [ "$(git rev-list HEAD...origin/master --count)" -gt "0" ]; then
   echo "Updating CrewLink-Server"
   service crewlink-* stop
   su -c "git fetch --all;git reset --hard origin/master" crewlink
   chown -R crewlink:crewlink $base
   service crewlink-* start
-  if [[ "$(journalctl -u crewlink-9736 --since=-1h | tail -n 4)" =~ .*"CrewLink Server started".* ]]; then
+  if [[ "$(journalctl -u crewlink-\*.service --since=-1h | tail -n 4)" =~ .*"CrewLink Server started".* ]]; then
     echo "CrewLink Server started."
     status=0
   else
